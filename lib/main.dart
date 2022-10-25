2c604c97850f528a4d04a684897acd96fce240f5
Kaio C. de Oliveira <hello@kaio.dev>
2022-10-25 10:35:58 -0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Responsy',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.teal,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
